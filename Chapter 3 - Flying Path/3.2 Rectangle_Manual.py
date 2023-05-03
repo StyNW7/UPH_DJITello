ef9ff516965a9dd595e0eb06aa8abb0471bf2be6
Clay Hinskie <132320728+ChClay@users.noreply.github.com>
2023-05-03 14:50:25 +0700

    Create 3.2 Rectangle_Manual.py

---
+++ Chapter 3 - Flying Path/3.2 Rectangle_Manual.py
@@ -0,0 +1,16 @@
+from djitellopy import Tello
+tello = Tello()
+tello.connect()
+
+tello.takeoff()
+
+tello.move_forward(30) # Moves forward 30cm
+tello.rotate_counter_clockwise(90) # rotate 90 degrees counterclockwise
+tello.move_forward(50) # Moves forward 30cm
+tello.rotate_counter_clockwise(90) # rotate 90 degrees counterclockwise
+tello.move_forward(30) # Moves forward 30cm
+tello.rotate_counter_clockwise(90) # rotate 90 degrees counterclockwise
+tello.move_forward(50) # Moves forward 30cm
+tello.rotate_counter_clockwise(90) # rotate 90 degrees counterclockwise
+
+tello.land()
