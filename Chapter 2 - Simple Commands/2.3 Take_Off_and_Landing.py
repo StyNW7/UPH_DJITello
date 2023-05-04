86fba7cefc5eb60f6e97146bbd374f0a48a99d62
Stanley Nathanael Wijaya <stanley.n.wijaya7@gmail.com>
2023-05-04 07:47:25 +0700

    Update 2.3 Take_Off_and_Landing.py

--- Chapter 2 - Simple Commands/2.3 Take_Off_and_Landing.py
+++ Chapter 2 - Simple Commands/2.3 Take_Off_and_Landing.py
@@ -1,3 +1,9 @@
+from djitellopy import Tello # import library
+
+tello = Tello() # define object
+
+tello.connect() # make a connection
+
 tello.takeoff() # Takeoff Command
 
 tello.land() # Land Command
