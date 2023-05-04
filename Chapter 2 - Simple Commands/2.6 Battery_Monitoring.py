adb5811a08d2f908130beee7f20ea01a5709ca9c
Stanley Nathanael Wijaya <stanley.n.wijaya7@gmail.com>
2023-05-04 07:49:05 +0700

    Update 2.6 Battery_Monitoring.py

--- Chapter 2 - Simple Commands/2.6 Battery_Monitoring.py
+++ Chapter 2 - Simple Commands/2.6 Battery_Monitoring.py
@@ -1,1 +1,5 @@
+from djitellopy import Tello # import library
+tello = Tello() # define object
+tello.connect() # make a connection
+
 print(tello.get_battery()) # display the information of the battery status in the output section.
