662f47811544d4825596aa1fccb4b952963725b1
Stanley Nathanael Wijaya <stanley.n.wijaya7@gmail.com>
2023-05-09 13:14:50 +0700

    Update 3.5 Zig_Zag.py

--- Chapter 3 - Flying Path/3.5 Zig_Zag.py
+++ Chapter 3 - Flying Path/3.5 Zig_Zag.py
@@ -1,12 +1,16 @@
 from djitellopy import Tello
+
 tello = Tello()
 tello.connect()
 
-tello.takeoff() # Takes off
-tello.rotate_clockwise(90) # Rotates 90° clockwise
-tello.move_forward(50) # Moves 50cm forward
-tello.rotate_counter_clockwise(135) # Rotates 135° counter clockwise
-tello.move_forward(50) # Moves 50cm forward
-tello.rotate_clockwise(135) # Rotates 135° clockwise
-tello.move_forward(50) # Moves 50cm forward
-tello.land() # Lands
+tello.takeoff()
+tello.rotate_clockwise(45)
+tello.move_forward(50)
+tello.rotate_counter_clockwise(90)
+tello.move_forward(50)
+tello.rotate_clockwise(90)
+tello.move_forward(50)
+tello.rotate_counter_clockwise(90)
+tello.move_forward(50)
+
+tello.land()
