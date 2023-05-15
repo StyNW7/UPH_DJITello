3c59e7b95a645f690c3fbf63796d12ce148e5a5f
Stanley Nathanael Wijaya <stanley.n.wijaya7@gmail.com>
2023-05-15 14:21:47 +0700

    Create 4.3.3 Example of Controlling Proximity Sensor Inside The Tello Drone.py

---
+++ Chapter 4 - Sensors/4.3.3 Example of Controlling Proximity Sensor Inside The Tello Drone.py
@@ -0,0 +1,42 @@
+# Import necessary libraries
+from djitellopy import Tello
+import time
+
+# Connect to Tello drone
+tello = Tello()
+tello.connect()
+
+tello.takeoff()
+
+# variables
+
+height = 0
+newHeight = 0
+sameHeight = True
+
+# while loop condition and comparasion between first height and new height after forward x cm
+
+while sameHeight:
+
+    height = tello.get_distance_tof()
+
+    print("Height: " + str(height))
+    time.sleep(0.5)
+
+    tello.move_forward(100)
+
+    time.sleep(2)
+
+    newHeight = tello.get_distance_tof()
+
+    print("New Height: " + str(newHeight))
+
+    if newHeight == height:
+        tello.move_forward(100)
+    else:
+        tello.move_up(50)
+        sameHeight = False
+        break
+
+tello.land()
+tello.end()
