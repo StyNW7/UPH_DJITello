d470a5b7eee45907989a27b8d52478bfb9527a38
Stanley Nathanael Wijaya <stanley.n.wijaya7@gmail.com>
2023-05-15 14:16:43 +0700

    Create Google_Colaboratory_Unzip_File.py

---
+++ Chapter 5 - Machine Learning/Google_Colaboratory_Unzip_File.py
@@ -0,0 +1,2 @@
+Original command: !unzip -q ../”ZIP FOLDER NAME.zip” -d ../ # change the "ZIP FOLDER NAME" to your ZIP folder name
+Command: !unzip -q ../MaskDataSet.zip -d ../ # this is for the example if the ZIP folder name is, "MaskDataSet.zip"
