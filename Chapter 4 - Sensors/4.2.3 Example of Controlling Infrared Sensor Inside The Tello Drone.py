5058c6934f7f9c7e3b0588bae31714672185492e
Stanley Nathanael Wijaya <stanley.n.wijaya7@gmail.com>
2023-05-22 14:06:41 +0700

    Update 4.2.3 Example of Controlling Infrared Sensor Inside The Tello Drone.py

--- Chapter 4 - Sensors/4.2.3 Example of Controlling Infrared Sensor Inside The Tello Drone.py
+++ Chapter 4 - Sensors/4.2.3 Example of Controlling Infrared Sensor Inside The Tello Drone.py
@@ -1,9 +1,10 @@
-# Import necessary libraries
+# Import necessary libraries 
 from djitellopy import Tello
 import time
+import cv2
 
-# Connect to Tello drone
-tello = Tello()
+# Connect to Tello drone 
+tello = Tello() 
 tello.connect()
 
 tello.takeoff()
@@ -14,20 +15,27 @@ print(height)
 
 # Check for obstacles
 
-if height <= 50:
-    tello.move_up(20) # Move up if obstacle detected
-    time.sleep(1) # Wait for the obstacle to pass
-else:
-    tello.move_forward(20) # Move forward if no obstacle detected
+while True:
 
-# Maintain the same height
+    if height <= 50: 
+        tello.move_up(20) # Move up if obstacle detected 
+        time.sleep(1) # Wait for the obstacle to pass 
+    else: 
+        tello.move_forward(100) # Move forward if no obstacle detected
 
-if height >= 100:
-    tello.move_down(20)
-elif height <= 80:
-    tello.move_up(20)
+    print(height)
 
-print(height)
+    # Maintain the same height
 
-tello.land()
+    if height >= 100: 
+        tello.move_down(20) 
+    elif height <= 80: 
+        tello.move_up(20)
+
+    print(height)
+
+    if cv2.waitKey(1) == 27: #ESC
+        break
+
+tello.land() 
 tello.end()
